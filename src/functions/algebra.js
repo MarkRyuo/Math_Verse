459980dddddff98c529f68055f7b0cd2aadbd65b
MarkRyuo <malupajhonmark3035@gmail.com>
2025-09-19 11:53:20 +0800

    Subject: Update 09/19/25

---
+++ src/functions/algebra.js
@@ -0,0 +1,8 @@
+
+
+
+// main 
+
+export const handlerAlgebra = () => {
+    
+};
---
+++ src/pages/algebra_page.js
@@ -0,0 +1,11 @@
+
+
+
+export function algebraPage() {
+    document.querySelector("#app").innerHTML = `
+        <div>
+            <input type="text" name="myInput" placeholder="Enter a number" size="30" required>
+            <input type="text" name="myInput" placeholder="Enter a number" size="30" required>
+        </div>
+    `
+}
