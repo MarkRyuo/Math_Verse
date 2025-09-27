240b50da5690a6143d18908406391f5af58d5ee3
MarkRyuo <malupajhonmark3035@gmail.com>
2025-09-27 22:13:53 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -16,12 +16,12 @@ export function homepage() {
         </div>
 
         <div>
-          <button type="button" name="abstract" id="" class="">Abs</button>
-          <button type="button" name="abstract" id="" class="">Abs</button>
-          <button type="button" name="abstract" id="" class="">Abs</button>
-          <button type="button" name="abstract" id="" class="">Abs</button>
-          <button type="button" name="abstract" id="" class="">Abs</button>
-          <button type="button" name="abstract" id="" class="">Abs</button>
+          <button type="button" name="abstract" id="" class="">Absolute</button>
+          <button type="button" name="abstract" id="" class="">Ceiling</button>
+          <button type="button" name="abstract" id="" class="">Floor</button>
+          <button type="button" name="abstract" id="" class="">Round</button>
+          <button type="button" name="abstract" id="" class="">Truncate</button>
+          <button type="button" name="abstract" id="" class="">Sign</button>
         </div>
       </main>
     </div>
