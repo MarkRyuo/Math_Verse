20fcb69aa9349dd40829bdcb0080fb3d2c3da188
MarkRyuo <malupajhonmark3035@gmail.com>
2025-09-27 22:16:04 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -10,18 +10,19 @@ export function homepage() {
       </nav>
 
       <main>
+        <h1>Converter</h1>
         <div>
           <input type="text" name="myInput" placeholder="Enter a number" size="30" required>
           <input type="text" name="myInput" placeholder="Enter a number" size="30" required>  
         </div>
 
         <div>
-          <button type="button" name="abstract" id="" class="">Absolute</button>
-          <button type="button" name="abstract" id="" class="">Ceiling</button>
-          <button type="button" name="abstract" id="" class="">Floor</button>
-          <button type="button" name="abstract" id="" class="">Round</button>
-          <button type="button" name="abstract" id="" class="">Truncate</button>
-          <button type="button" name="abstract" id="" class="">Sign</button>
+          <button type="button" name="abstract" id="btnAbs" class="">Absolute</button>
+          <button type="button" name="abstract" id="btnCeiling" class="">Ceiling</button>
+          <button type="button" name="abstract" id="btnFloor" class="">Floor</button>
+          <button type="button" name="abstract" id="btnRound" class="">Round</button>
+          <button type="button" name="abstract" id="btnTruncate" class="">Truncate</button>
+          <button type="button" name="abstract" id="btnSign" class="">Sign</button>
         </div>
       </main>
     </div>
