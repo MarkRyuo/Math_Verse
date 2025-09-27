053219d2ad29ce7ee013ad89d91468f6069ce2a5
MarkRyuo <malupajhonmark3035@gmail.com>
2025-09-27 22:11:14 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -4,7 +4,27 @@ import { router } from './router'
 export function homepage() {
   document.querySelector('#app').innerHTML = `
   <div>
-    <h1>Homepage</h1>
+    <div>
+      <nav>
+
+      </nav>
+
+      <main>
+        <div>
+          <input type="text" name="myInput" placeholder="Enter a number" size="30" required>
+          <input type="text" name="myInput" placeholder="Enter a number" size="30" required>  
+        </div>
+
+        <div>
+          <button type="button" name="abstract" id="" class="">Abs</button>
+          <button type="button" name="abstract" id="" class="">Abs</button>
+          <button type="button" name="abstract" id="" class="">Abs</button>
+          <button type="button" name="abstract" id="" class="">Abs</button>
+          <button type="button" name="abstract" id="" class="">Abs</button>
+          <button type="button" name="abstract" id="" class="">Abs</button>
+        </div>
+      </main>
+    </div>
   </div>
 `
 }
--- src/router.js
+++ src/router.js
@@ -1,13 +1,11 @@
 import Navigo from "navigo";
 import { homepage } from "./main";
-import  algebraPage  from "./pages/algebra_page.js";
 
 
 export const router = new Navigo('/') ;
 
 router 
     .on("/",  () => homepage() )
-    .on("/algebraPage", () => algebraPage() )
     .notFound(() => {
     document.querySelector("#app").innerHTML = `<h1>404 Page Not Found!</h1>`;
   });
