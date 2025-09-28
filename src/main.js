dfa581b9dab62ca3a5a02f18b254032dbba4df44
John Matthew K. Tizon <116562606+Matthewtizon@users.noreply.github.com>
2025-09-28 16:59:05 +0800

    kopals (#6)

--- src/main.js
+++ src/main.js
@@ -30,6 +30,7 @@ export function homepage() {
           <button type="button" name="devide" id="devide" class"">/</button>
           <button type="button" name="devide" id="devide" class"">Kupal</button>
           <button type="button" name="devide" id="devide" class"">tite</button>
+          <button type="button" name="devide" id="devide" class"">kopals</button>
         </div>
       </main>
     </div>
